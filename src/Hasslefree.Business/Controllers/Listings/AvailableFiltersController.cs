2a85801ff6db11c45bcc1b4e6bf4d3ac5e30b1e1
Uwan <uwan@cloudmeg.co.za>
2021-09-01 19:40:51 +0200

    - latest

---
+++ src/Hasslefree.Business/Controllers/Listings/AvailableFiltersController.cs
@@ -0,0 +1,24 @@
+﻿using Hasslefree.Web.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hasslefree.Business.Controllers.Listings
+{
+	public class AvailableFiltersController : BaseController
+	{
+		[ChildActionOnly]
+		public ActionResult GetFilters(List<PropertyListItem)
+		{
+			var model = FilterService.WithPath(path).List();
+
+			ViewBag.Title = $"Browse Listings - {model.CategoryName}";
+
+			//get category
+
+			return View("../Listings/List", model);
+		}
+	}
+}
