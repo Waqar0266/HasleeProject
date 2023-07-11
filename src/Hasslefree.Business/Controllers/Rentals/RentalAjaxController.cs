a226db8a958905e1c40d5986afc1f43038cf2d2b
Uwan <uwan@cloudmeg.co.za>
2023-07-11 11:43:50 +0200

    - fixed auto complete

--- src/Hasslefree.Business/Controllers/Rentals/RentalAjaxController.cs
+++ src/Hasslefree.Business/Controllers/Rentals/RentalAjaxController.cs
@@ -2,32 +2,33 @@
 using Hasslefree.Data;
 using Hasslefree.Web.Framework;
 using Hasslefree.Web.Framework.Filters;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
 namespace Hasslefree.Business.Controllers.Rentals
 {
-	[AccessControlFilter(Permission = "Agent,Director")]
-	public class RentalAjaxController : BaseController
-	{
-		//Repos
-		private IReadOnlyRepository<Rental> RentalRepo { get; }
+    [AccessControlFilter(Permission = "Agent,Director")]
+    public class RentalAjaxController : BaseController
+    {
+        //Repos
+        private IReadOnlyRepository<Rental> RentalRepo { get; }
 
-		public RentalAjaxController(
-			//Repos
-			IReadOnlyRepository<Rental> rentalRepo
-			)
-		{
-			//Repos
-			RentalRepo = rentalRepo;
-		}
+        public RentalAjaxController(
+            //Repos
+            IReadOnlyRepository<Rental> rentalRepo
+            )
+        {
+            //Repos
+            RentalRepo = rentalRepo;
+        }
 
-		[HttpGet, Route("rentals/query")]
-		public ActionResult Index(string phrase)
-		{
-			var rentals = RentalRepo.Table.ToList();
-			var results = rentals.Where(a => a.Premises.ToLower().Contains(phrase) || a.Address.ToLower().Contains(phrase)).Select(x => new { RentalId = x.RentalId, Name = x.Premises + " (" + x.Address + ")" }).ToList();
-			return Json(results, JsonRequestBehavior.AllowGet);
-		}
-	}
+        [HttpGet, Route("rentals/query")]
+        public ActionResult Index(string phrase)
+        {
+            var rentals = RentalRepo.Table.ToList();
+            var results = rentals.Where(a => (!String.IsNullOrEmpty(a.Premises) && a.Premises.ToLower().Contains(phrase)) || (!String.IsNullOrEmpty(a.Address) && a.Address.ToLower().Contains(phrase))).Select(x => new { RentalId = x.RentalId, Name = x.Premises + " (" + x.Address + ")" }).ToList();
+            return Json(results, JsonRequestBehavior.AllowGet);
+        }
+    }
 }
