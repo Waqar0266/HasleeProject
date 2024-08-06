7996e9cb17ac10ab6d21dbe62656e508bf4ae8a6
Waqar0266 <waqar0781@gmail.com>
2024-08-06 13:01:53 +0500

    Test

--- src/Hasslefree/Web/Models/Agents/AgentDocumentModel.cs
+++ src/Hasslefree/Web/Models/Agents/AgentDocumentModel.cs
@@ -9,5 +9,7 @@ namespace Hasslefree.Web.Models.Agents
 		public double Size { get; set; }
 		public string Path { get; set; }
 		public DateTime CreatedOn { get; set; }
+		public string Extension { get; set; }
+		public byte[] Binary { get; set; }
 	}
 }
